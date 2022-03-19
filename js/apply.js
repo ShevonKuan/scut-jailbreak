a018dfa471428373f3f0697c4505dd29bc660405
Dnomd343 <dnomd343@163.com>
2022-03-20 00:15:07 +0800

    update: modify by shevon

--- js/apply.js
+++ js/apply.js
@@ -10,29 +10,65 @@ var Info = {
         { label: "学院", value: "-" },
         { label: "年级", value: "-" },
         { label: "人员类型", value: "-" }
-	],
+    ],
     stepBox: [ // 步骤栏
-        { "name": "提交", "url": "img/unTrue.png" },
-        { "name": "院级审批", "url": "img/unTrue.png" },
-        { "name": "校级审批", "url": "img/unTrue.png" }
+        { "name": "提交", "url": "img/true.png" },
+        { "name": "院级审批", "url": "img/true.png" },
+        { "name": "校级审批", "url": "img/true.png" }
     ]
 }
 
+
+function completeDate(value) {
+    return value < 10 ? "0" + value : value;
+}
+
+function getNowFormatDay(nowDate) {
+    var char = "-";
+    if (nowDate == null) {
+        nowDate = new Date();
+    }
+    var day = nowDate.getDate();
+    var month = nowDate.getMonth() + 1;
+    var year = nowDate.getFullYear();
+    return year + char + completeDate(month) + char + completeDate(day);
+}
 var Global_FN = {
     init() {
         layui.use(['jquery'], function() {
-            if (Global_FN.getQuery('fill') === 'true') {
-                Global_FN.fillScutInfo();
+            let $ = layui.jquery;
+            var data;
+            if (location.hash === '') {
+                data = $.getJSON({ url: "api/jailbreak/student/", async: false });
+            } else {
+                data = $.getJSON({ url: "api/jailbreak/student/" + location.hash.replace('#', ''), async: false });
+                if (data['status'] === 'failed') {
+                    data = $.getJSON({ url: "api/jailbreak/student/", async: false });
+                }
             }
+            data = data.responseJSON
+            console.log(data)
+            Info.scutInfo[0].value = data['data']['name'];
+            Info.scutInfo[1].value = data['data']['id'];
+            Info.scutInfo[2].value = data['data']['college'];
+            Info.scutInfo[3].value = data['data']['class'];
+            Info.scutInfo[4].value = '本科生'
+            var date = new Date()
+            $(".dOutStartDate").text(getNowFormatDay());
+            $(".dOutEndDate").text(getNowFormatDay());
+            $("#reason").text(data['data']['reason']);
+            $("#addr").val(data['data']['addr']);
+
+            $("input[name$='scutName']").val(data['data']['name']);
+            $("input[name$='scutCode']").val(data['data']['id']);
+            $("input[name$='scutCollege']").val(data['data']['college']);
+            $("input[name$='scutGrade']").val(data['data']['class']);
+            $("input[name$='scutDegree']").val('本科生');
+
             Global_FN.fillStepBox();
             Global_FN.initDatePicker();
+            
         });
-    },
-    getQuery(param) { // 获取URL参数
-        var reg = new RegExp("(^|&)" + param + "=([^&]*)(&|$)");
-        var r = window.location.search.substr(1).match(reg);
-        if (r === null) { return null; }
-        return decodeURI(r[2]);
     },
     fillStepBox() { // 渲染进度栏
         layui.use(["jquery"], function() {
@@ -42,7 +78,7 @@ var Global_FN = {
             $.each(Info.stepBox, (index, item) => {
                 let sLine = "";
                 index != Info.stepBox.length - 1 ? sLine = "line" : "";
-                sList += '<div class="step '+ sLine +'"><img src="'+ item.url +'"><span>'+ item.name +'</span></div>'
+                sList += '<div class="step ' + sLine + '"><img src="' + item.url + '"><span>' + item.name + '</span></div>'
             })
             $(".step_box").html(sList);
         });
@@ -60,7 +96,7 @@ var Global_FN = {
                     end: new Date().getFullYear() + 1,
                     onConfirm: function(result) {
                         var mm = "",
-                        dd = "";
+                            dd = "";
                         result[1].value.toString().length === 1 ? mm = "0" + result[1].value : mm = result[1].value;
                         result[2].value.toString().length === 1 ? dd = "0" + result[2].value : dd = result[2].value;
                         $('.' + value).text(result[0].value + "-" + mm + "-" + dd);
@@ -71,31 +107,17 @@ var Global_FN = {
         });
     },
     fillScutInfo() { // 填充数据
-        layui.use(['form','jquery'], function() {
+        layui.use(['form', 'jquery'], function() {
             let $ = layui.jquery;
 
             Info.stepBox[0].url = "img/true.png";
             Info.stepBox[1].url = "img/true.png";
             Info.stepBox[2].url = "img/true.png";
 
-            $(".dOutStartDate").text(Global_FN.getQuery('start') || '1970-01-01');
-            $(".dOutEndDate").text(Global_FN.getQuery('end') || '1970-01-01');
-
-            layui.form.val('info_form', {
-                sOutAddress: Global_FN.getQuery('address') || '',
-                sOutReason: Global_FN.getQuery('reason') || '',
-            });
-
-            Info.scutInfo[0].value = Global_FN.getQuery('name') || '-';
-            Info.scutInfo[1].value = Global_FN.getQuery('code') || '-';
-            Info.scutInfo[2].value = Global_FN.getQuery('college') || '-';
-            Info.scutInfo[3].value = Global_FN.getQuery('grade') || '-';
-            Info.scutInfo[4].value = Global_FN.getQuery('degree') || '-';
-
             let htmlContent = "";
             $(".info_box").empty();
             $.each(Info.scutInfo, (_index, item) => {
-                htmlContent += '<div class="info_list"><span>'+ item.label +'</span><span>'+ item.value +'</span></div>';
+                htmlContent += '<div class="info_list"><span>' + item.label + '</span><span>' + item.value + '</span></div>';
             })
             $(".info_box").html(htmlContent);
 
@@ -103,28 +125,10 @@ var Global_FN = {
             $(".btn_add").hide();
             $(".logo_box").show();
             $(".info_input").hide();
-            $(".logo_box img").attr("src" ,"./img/pass.png");
+            $(".logo_box img").attr("src", "./img/pass.png");
         });
     },
     sub() {
-        let $ = layui.jquery;
-        let formData = layui.form.val("info_form");
-        var info = {
-            'start': $(".dOutStartDate").text(),
-            'end': $(".dOutEndDate").text(),
-            'address': formData['sOutAddress'],
-            'reason': formData['sOutReason'],
-            'name': formData['scutName'],
-            'code': formData['scutCode'],
-            'college': formData['scutCollege'],
-            'grade': formData['scutGrade'],
-            'degree': formData['scutDegree'],
-        }
-        let query = '?fill=true&';
-        $.each(info, (index, item) => {
-            query += index + '=' + item + '&'
-        })
-        query = query.substr(0, query.length - 1);
-        window.location.href = 'apply.html' + query;
+        Global_FN.fillScutInfo();
     }
 }
